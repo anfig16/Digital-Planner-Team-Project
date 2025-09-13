8c524cc6c78ab69dc837825732729b306b7c720d
anfig16 <113375454+anfig16@users.noreply.github.com>
2025-09-13 14:23:32 -0500

    Create DigitalPlannerApplication.java

---
+++ src/test/java/com/planner/digitalPlanner/DigitalPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.planner.digitalPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
